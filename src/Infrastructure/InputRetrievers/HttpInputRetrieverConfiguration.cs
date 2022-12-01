e72bb6533430abf6f7450144e35fc9c900c5a8fb
indomitable <89315854+696e646f6d697461626c65@users.noreply.github.com>
2022-12-01 18:47:04 +0100

    Add section name to http input retriever config

--- src/Infrastructure/InputRetrievers/HttpInputRetrieverConfiguration.cs
+++ src/Infrastructure/InputRetrievers/HttpInputRetrieverConfiguration.cs
@@ -2,5 +2,6 @@ namespace AOC2022.Infrastructure.InputRetrievers;
 
 public class HttpInputRetrieverConfiguration
 {
+    public const string Name = "Http";
     public required string SessionCookie { get; init; }
 }
